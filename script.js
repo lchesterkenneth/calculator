73b8c6813db473cb1c7024a43c800bca3be6b67a
lchesterkenneth <lchesterkenneth@gmail.com>
2026-05-03 20:58:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById("display");
+
+// Add value to screen
+function appendValue(value) {
+    display.value += value;
+}
+
+// clear screen
+function clearDisplay() {
+
+    display.value = "";
+}
+
+// Delete last character
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Calculate result
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
